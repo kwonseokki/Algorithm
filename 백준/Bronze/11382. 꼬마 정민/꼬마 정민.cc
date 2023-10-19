1d34a55061ab0f9e3a4d7a5a42917e3f1ba62c3c
Seokki Kwon <101062450+seokki96@users.noreply.github.com>
2023-10-20 00:29:41 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+long long int a, b, c;
+long long int s;
+int main(){
+    cin >> a >> b >> c;
+    s = a + b + c;
+    cout << s << "\n";
+    return 0;
+}
