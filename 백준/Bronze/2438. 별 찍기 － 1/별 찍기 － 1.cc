838f53cfb917bdf58a927b541409cf44e3df5d24
Seokki Kwon <101062450+seokki96@users.noreply.github.com>
2023-10-22 01:25:03 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n;
+string a;
+int main() {
+    cin >> n;
+    for(int i = 0; i < n; i++) {
+        a += "*";
+        cout << a << "\n";
+    }
+    return 0;
+}
