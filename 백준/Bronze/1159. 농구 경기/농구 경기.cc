9d2925abf972a5931f58159265720ed2477ba804
Seokki Kwon <101062450+seokki96@users.noreply.github.com>
2023-10-27 18:38:24 +0900

    [Bronze II] Title: 농구 경기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/1159. 농구 경기/농구 경기.cc
+++ 백준/Bronze/1159. 농구 경기/농구 경기.cc
@@ -1,21 +1,18 @@
 #include<bits/stdc++.h>
 using namespace std;
-string a;
-int b[26] = {0};
-vector<char> v;
 int n;
+int cnt[26];
+string a, ret;
 
 int main(){
-    cin >> n;
-    for(int i = 0; i < n; i++){
-        cin >> a;
-        b[(int) a[0] - 97] += 1;
-    }
-    for(int i = 0; i < 26; i++){
-        if(b[i] >= 5)v.push_back((char) i + 97);
-    }
-    if(v.size() > 0) for(char i : v) cout << i;
-    else cout << "PREDAJA";
-    cout << "\n";
-    return 0;
+   cin >> n;
+   for(int i = 0; i < n; i++){
+      cin >> a;
+      cnt[a[0] - 'a']++;
+   }
+   for(int i = 0; i < 26; i++)if(cnt[i] >= 5) ret += i + 'a';
+   if(ret.size()) cout << ret;
+   else cout << "PREDAJA";
+   cout << '\n';
+   return 0;
 }
