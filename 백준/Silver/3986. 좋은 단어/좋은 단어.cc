c6dd396c5850a860d26fe0e114094db247720b22
Seokki Kwon <101062450+seokki96@users.noreply.github.com>
2023-10-28 20:47:42 +0900

    [Silver IV] Title: 좋은 단어, Time: 4 ms, Memory: 2392 KB -BaekjoonHub

--- 백준/Silver/3986. 좋은 단어/좋은 단어.cc
+++ 백준/Silver/3986. 좋은 단어/좋은 단어.cc
@@ -1,19 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
-vector<char> v;
-int n, cnt;
+int n, ret;
 string s;
+
 int main(){
+   ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
    cin >> n;
-   for(int i = 0; i < n; i++){      
+   for(int i = 0; i < n; i++){
+      stack<char> stk;
       cin >> s;
-      for(int i = 0; i < s.size(); i++){
-         if(v.size() > 0 && v.back() == s[i]) v.pop_back();
-         else v.push_back(s[i]);
+      for(int j = 0; j < s.size(); j++){
+         if(stk.size() && stk.top() == s[j])stk.pop();
+         else stk.push(s[j]);
       }
-      if(v.size() == 0)cnt++;
-      v.clear();
+      if(stk.size() == 0) ret++;
    }
-   cout << cnt << "\n";
+   cout << ret << "\n";
    return 0;
 }
