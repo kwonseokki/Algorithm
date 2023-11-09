487f6d43c91b6319d573939564ecafcdd9e49088
Seokki Kwon <kwons4163@naver.com>
2023-11-10 06:20:09 +0900

    [Silver III] Title: 팰린드롬 만들기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.cc
+++ 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.cc
@@ -1,35 +1,29 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-string s, ret, temp;
-int cnt[123], flag;
+string s, temp, ret;
+int cnt, a[26];
 
-int main(){
-   ios_base::sync_with_stdio(false);
-   cin.tie(NULL);
+int main()
+{
    cin >> s;
-   for(int ch : s)cnt[ch]++;
-   for(int i = 'Z'; i >= 'A'; i--){
-      if(cnt[i]){
-         // 홀수인경우
-        if(cnt[i] & 1){
-            temp = i;
-            cnt[i]--;
-            flag++;
-        }
-        // 홀수가 2개인 경우 팰린드롬이 아님
-        if(flag >= 2) break;
+   for(char i : s)a[i - 65] += 1;
+   for(int i = 'Z'; i >= 'A'; i--) {
+      if(cnt > 1) break;
+      if(a[i - 65]){
+         for(int j = 0; j < a[i - 65] / 2; j++){
+            ret = ret + (char) i;
+            ret = (char) i + ret;
+         }
       }
-      // 문자열 합치기
-      for(int j = 0; j < cnt[i]; j += 2){
-         ret = ret + (char) i;
-         ret = (char) i + ret;
+      if(a[i - 65] % 2 != 0){
+         temp = (char) i;
+         cnt++;
       }
    }
-   // 홀수개 문자열이 1개인경우 합치기
-   if(temp.size() != 0){
-      ret = ret.substr(0, ret.size() / 2) + temp + ret.substr(ret.size() / 2);
+   if(cnt >  1)cout << "I'm Sorry Hansoo" << "\n";
+   else {
+      ret = ret.substr(0, ret.size() / 2) + temp + ret.substr(ret.size() / 2, ret.size());
+      cout << ret << '\n';
    }
-   if(flag >= 2) cout << "I'm Sorry Hansoo" << "\n";
-   else cout << ret << '\n';
    return 0;
 }
