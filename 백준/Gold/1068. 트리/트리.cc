1d37fe4ff7b8b4c04620a8271d7dacebbb35fafd
Seokki Kwon <kwons4163@naver.com>
2023-12-08 11:19:14 +0900

    [Gold V] Title: 트리, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1068. 트리/트리.cc
+++ 백준/Gold/1068. 트리/트리.cc
@@ -1,33 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n, r, root, visited[51];
+int n, _index, root, _remove;
 vector<int> adj[51];
 
-int dfs(int here){
-    if(adj[here].size() == 1 && r == adj[here][0]) return 1;
-    if(r == here) return 0;
-    if(adj[here].size() == 0) return 1;
-    visited[here] = 1;
+int dfs(vector<int> here){
+    if(here.size() == 1 && _remove == here[0]) return 1;
+    if(!here.size()) return 1;
     int ret = 0;
-    for(int there: adj[here]){
-        if(there == r || visited[there]) continue;
-        ret += dfs(there);
+    for(int there: here){
+        if(there == _remove) continue;
+        ret += dfs(adj[there]);
     }
     return ret;
 }
 
-int main(){
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+int main() {
     cin >> n;
     for(int i = 0; i < n; i++){
-        int index;
-        cin >> index;
-        adj[index].push_back(i);
-        if(index == -1) root = i;
+        cin >> _index;
+        if(_index == -1) {
+            root = i; 
+            continue;
+            }
+        adj[_index].push_back(i);
     }
-    cin >> r;
-    cout << dfs(root) << '\n';
+    cin >> _remove;
+    if(_remove == root) cout << 0 << '\n';
+    else cout << dfs(adj[root]) << '\n';
     return 0;
 }
