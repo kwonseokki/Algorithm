e2ad58862358422eff1859f0a3f0713229d3529a
Seokki Kwon <kwons4163@naver.com>
2023-12-09 15:48:59 +0900

    [Silver IV] Title: 주몽, Time: 80 ms, Memory: 2080 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.cc
+++ 백준/Silver/1940. 주몽/주몽.cc
@@ -1,24 +1,19 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int n, m, cnt, a[15001];
+int n, m, a[15001], ret;
 
+int main() {
+    ios::sync_with_stdio(false);
+    cout.tie(NULL); cin.tie(NULL);
+    cin >> n;
+    cin >> m;
+    for(int i = 0; i < n; i++)cin >> a[i];
+    for(int i = 0; i < n; i++){
+        for(int j = i + 1; j < n; j++){
+            if(a[i] + a[j] == m)ret++;
+        }
+    }
+    cout << ret << '\n';
+    return 0;
 
-int main(){
-   ios_base::sync_with_stdio(false);
-	cin.tie(NULL); cout.tie(NULL);
-   cin >> n;
-   cin >> m;
-   for(int i = 0; i < n; i++){
-      cin >> a[i];
-   }
-   if(m > 200000) cout << 0 << '\n';
-   else{
-      for(int i = 0; i < n; i++){
-         for(int j = 0; j < i; j++){
-           if(a[i] + a[j] == m) cnt++;
-         }
-      }
-      cout << cnt << "\n";
-   }
-   return 0;
 }
